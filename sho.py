8e6933d369ecc7a1fb9e4da10123d05092bfc746
skyyriiz <80923101+skyyriiz@users.noreply.github.com>
2023-01-24 15:44:00 +0100

    Switching on linux device
    Imma try shodan cmd line

--- sho.py
+++ sho.py
@@ -1,7 +1,6 @@
 import shodan
 import argparse
 import os
-from dotenv import load_dotenv
 
 parser = argparse.ArgumentParser(
                     prog = 'ProgramName',
@@ -17,38 +16,29 @@ api_key = shodan.Shodan("7daDcD0wcDnczyFX2E4jIHoDG41s2iZB");
 
 print(api_key);
 
-# Wrap the request in a try/ except block to catch errors
-try:
-        # Search Shodan
-        results = api_key.search('esgi.fr')
+os.system("pip install setuptools")
 
-        # Show the results
-        print('Results found: {}'.format(results['total']))
-        for result in results['matches']:
-                print('IP: {}'.format(result['ip_str']))
-                print(result['data'])
-                print('')
-except shodan.APIError as e:
-        print('Error: {}'.format(e))
-        
-# Lookup the host
-host = api_key.host('195.25.230.205')
 
-# Print general info
-print("""
+
+
+
+"""
+if args.ip:
+        host = api_key.host(args.ip)
+        print(
         IP: {}
         Organization: {}
         Operating System: {}
-""".format(host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a')))
-
-# Print all banners
-for item in host['data']:
-        print("""
+        .format(host['ip_str'], host.get('org', 'n/a'), host.get('os', 'n/a')))
+        for item in host['data']:
+                print(
                 Port: {}
                 Banner: {}
+                .format(item['port'], item['data']))
 
-        """.format(item['port'], item['data']))
+elif args.domain:
+        print("Domain")
 
-
-
-
+else:
+        print("No arguments")
+"""
