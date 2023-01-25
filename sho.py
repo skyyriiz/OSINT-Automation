dd5612e01998d9a5b296c7296f517cd1a7856969
skyyriiz <80923101+skyyriiz@users.noreply.github.com>
2023-01-25 16:59:36 +0100

    Add Scan Vuln
    Now we can scan the network and show all the vulnerabilities of a network thanks to shodan

--- sho.py
+++ sho.py
@@ -9,6 +9,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-i', '--ip', type=str)
 parser.add_argument('-d', '--domain', type=str)
+parser.add_argument('-v', '--vuln', type=str)
 
 args = parser.parse_args()
 
@@ -17,7 +18,12 @@ api_key = shodan.Shodan("7daDcD0wcDnczyFX2E4jIHoDG41s2iZB");
 if args.ip:
         os.system("shodan host --save " + args.ip)
         os.system("shodan parse " + args.ip + ".json.gz")
-        os.system("shodan convert " + args.ip + ".json.gz images")
+        os.system("shodan convert " + args.ip + ".json.gz csv")
+        os.system("rm " + args.ip + ".json.gz")
+        os.system("shodan stats --facets vuln:" + args.vuln + " net:" + args.ip + "/24 > vuln_" + args.ip + ".txt")
+        os.system("")
+        print("Toutes les informations sur l'ip ciblé ont été enregistrées dans le fichier " + args.ip + ".csv")
+        
 elif args.domain:
         os.system("shodan host " + args.domain)
 else:
