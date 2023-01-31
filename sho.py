ebd48bc6fa69a57c9f7e5f39fe0681baf8c91323
skyyriiz <80923101+skyyriiz@users.noreply.github.com>
2023-01-31 15:28:11 +0100

    Done with ip / Staring working with domain

--- sho.py
+++ sho.py
@@ -1,6 +1,7 @@
 import shodan
 import argparse
 import os
+import socket
 
 parser = argparse.ArgumentParser(
                     prog = 'ProgramName',
@@ -15,17 +16,23 @@ args = parser.parse_args()
 
 api_key = shodan.Shodan("7daDcD0wcDnczyFX2E4jIHoDG41s2iZB");
 
-if args.ip:
+print(args.domain)
+
+if args.ip and args.domain:
+        print("You can't use -i and -d at the same time")
+elif args.ip:
+        os.system("shodan init " + api_key)
         os.system("shodan host --save " + args.ip)
         os.system("shodan parse " + args.ip + ".json.gz")
         os.system("shodan convert " + args.ip + ".json.gz csv")
         os.system("rm " + args.ip + ".json.gz")
         os.system("shodan stats --facets vuln:" + args.vuln + " net:" + args.ip + "/24 > vuln_" + args.ip + ".txt")
-        os.system("")
-        print("Toutes les informations sur l'ip ciblé ont été enregistrées dans le fichier " + args.ip + ".csv")
-        
+        print("Toutes les informations sur l'ip ciblé ont été enregistrées dans le fichier " + args.ip + ".csv et toutes ces vulnérabiltés sont enregistrées dans le fichier vuln_" + args.ip + ".txt")
 elif args.domain:
-        os.system("shodan host " + args.domain)
+        if args.domain.split(".")[0] == "www":
+                print(socket.gethostbyname(args.domain))
+        else:
+                print(socket.gethostbyname("www." + args.domain))
 else:
         print("No arguments")
 
