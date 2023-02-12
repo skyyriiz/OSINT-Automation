47cc26c1ba27dbf2a55994c6c6760f9b31ee4053
Goenae <61237031+Goenae@users.noreply.github.com>
2023-02-13 00:02:02 +0100

    Complete merge

--- install.py
+++ install.py
@@ -2,3 +2,4 @@ import os
 
 os.system("python -m pip install --upgrade pip")
 os.system("pip install python-dotenv")
+os.system("pip install argparse")
---
+++ install_theharvester.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/laramies/theHarvester.git
+cd theHarvester
+sudo pip3 install -r requirements/dev.txt
+sudo pip3 install -r requirements/base.txt
